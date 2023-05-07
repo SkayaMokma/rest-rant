74a74be778c920a41af279893568937604347276
SkayaMokma <skayamokma@outlook.com>
2023-05-07 15:49:57 -0400

    Part 3

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,9 @@
+const express = require('express')
 const router = require('express').Router()
-
-router.get('/', (req, res) => {
-    res.send('GET /places')
+const app = express()
+// GET /places
+app.get('/', (req, res) => {
+  res.render('places/index')
 })
 
 module.exports = router
